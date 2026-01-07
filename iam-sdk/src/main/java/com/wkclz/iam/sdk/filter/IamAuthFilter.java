3ce1b3065f40bc511c47427984f9e9378ecd40d2
wangkc <wkclz@qq.com>
2026-01-08 00:11:39 +0800

    权限拦截器

--- iam-sdk/src/main/java/com/wkclz/iam/sdk/filter/IamAuthFilter.java
+++ iam-sdk/src/main/java/com/wkclz/iam/sdk/filter/IamAuthFilter.java
@@ -13,11 +13,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class IamAuthFilter extends OncePerRequestFilter {
 
     @Autowired
