6ab7535fd0bd2f1c68d0b5bad574a49b0b6faf52
wangkc <wkclz@qq.com>
2026-01-29 00:48:29 +0800

    字典获取

--- iam-admin-ui/src/utils/dict.js
+++ iam-admin-ui/src/utils/dict.js
@@ -1,27 +1,65 @@
-import useDictStore from '@/store/modules/dict'
-import { getDicts } from '@/api/data'
+import { commonDictsList } from '@/api/common'
+
+// 缓存10分钟
+const cacheTime = 10 * 60 * 1000;
 
 /**
  * 获取字典数据
  */
 export function useDict(...args) {
-  const res = ref({})
+  const res = ref({});
   return (() => {
-    args.forEach((dictType, index) => {
-      res.value[dictType] = []
-      const dicts = useDictStore().getDict(dictType)
-      if (dicts) {
-        res.value[dictType] = dicts
+    // 无入参
+    if (!args || args.length === 0) {
+      return toRefs(res.value);
+    }
+
+    // 查缓存，若缓存已经存在，不再请求
+    const renew = [];
+    for (const type of args) {
+      const key = 'DICT_' + type;
+      const item = localStorage.getItem(key);
+      if (item) {
+        const sde = JSON.parse(item);
+        const now =  new Date().getTime();
+        if (now - sde.timestamp < cacheTime) {
+          res.value[type] = sde.enums;
+        } else {
+          renew.push(type);
+          res.value[type] = [];
+          localStorage.removeItem(key);
+        }
       } else {
-        useDictStore().setDict(dictType, [])
-        /*
-        getDicts(dictType).then(resp => {
-          res.value[dictType] = resp.data.map(p => ({ label: p.dictLabel, value: p.dictValue, elTagType: p.listClass, elTagClass: p.cssClass }))
-          useDictStore().setDict(dictType, res.value[dictType])
-        })
-        */
+        renew.push(type);
+        res.value[type] = [];
       }
-    })
-    return toRefs(res.value)
+    }
+    if (renew.length === 0) {
+      return toRefs(res.value);
+    }
+    const dictTypes = renew.join(',');
+    commonDictsList({dictType: dictTypes}).then(rt => {
+      const data = rt.data;
+      const types = Object.keys(data)
+      for (const type of types) {
+        // 加入到返回的 res
+        res.value[type] = data[type].map(p => ({
+          // 若 value 为数字，需要转成数字
+          value: isNaN(p.dictValue) ? p.dictValue : Number(p.dictValue),
+          label: p.dictLabel,
+          elTagType: p.elType,
+          elTagClass: p.cssClass,
+          description: p.description,
+          disabled: 1 - (p.enableFlag === undefined ? 1 : p.enableFlag)
+        }))
+        // 缓存
+        const stroage = {
+          timestamp: new Date().getTime(),
+          enums: res.value[type]
+        };
+        localStorage.setItem('DICT_' + type, JSON.stringify(stroage));
+      }
+    });
+    return toRefs(res.value);
   })()
 }
