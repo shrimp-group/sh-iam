68e9ddfcd124f8c2a2e1ee74fb3ddc93262b1c9b
wangkc <wkclz@qq.com>
2026-05-05 21:57:50 +0800

    完善 request 的下载方法

--- iam-admin-ui/src/utils/request.js
+++ iam-admin-ui/src/utils/request.js
@@ -124,30 +124,57 @@ service.interceptors.response.use(res => {
   }
 )
 
-// 通用下载方法
+// 通用下载方法【get】
 export function download(url, params, filename, config) {
   downloadLoadingInstance = ElLoading.service({ text: "正在下载数据，请稍候", background: "rgba(0, 0, 0, 0.7)", })
-  return service.post(url, params, {
-    transformRequest: [(params) => { return tansParams(params) }],
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  const queryString = tansParams(params);
+  if (queryString !== '') {
+    url = url + "?" + queryString;
+  }
+  return service.get(url, {
     responseType: 'blob',
     ...config
   }).then(async (data) => {
-    const isBlob = blobValidate(data)
+    const isBlob = blobValidate(data);
     if (isBlob) {
       const blob = new Blob([data])
       saveAs(blob, filename)
     } else {
-      const resText = await data.text()
-      const rspObj = JSON.parse(resText)
+      const resText = await data.text();
+      const rspObj = JSON.parse(resText);
       const errMsg = errorCode[rspObj.code] || rspObj.msg || errorCode['default']
-      ElMessage.error(errMsg)
+      ElMessage.error(errMsg);
     }
-    downloadLoadingInstance.close()
+    downloadLoadingInstance.close();
   }).catch((r) => {
     console.error(r)
     ElMessage.error('下载文件出现错误，请联系管理员！')
-    downloadLoadingInstance.close()
+    downloadLoadingInstance.close();
+  })
+}
+// 通用下载方法【post】
+export function postDownload(url, params, filename, config) {
+  downloadLoadingInstance = ElLoading.service({ text: "正在下载数据，请稍候", background: "rgba(0, 0, 0, 0.7)", })
+  return service.post(url, params, {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    responseType: 'blob',
+    ...config
+  }).then(async (data) => {
+    const isBlob = blobValidate(data);
+    if (isBlob) {
+      const blob = new Blob([data])
+      saveAs(blob, filename)
+    } else {
+      const resText = await data.text();
+      const rspObj = JSON.parse(resText);
+      const errMsg = errorCode[rspObj.code] || rspObj.msg || errorCode['default']
+      ElMessage.error(errMsg);
+    }
+    downloadLoadingInstance.close();
+  }).catch((r) => {
+    console.error(r)
+    ElMessage.error('下载文件出现错误，请联系管理员！')
+    downloadLoadingInstance.close();
   })
 }
 
